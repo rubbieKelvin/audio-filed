af947dda831be5be4c036b0b6ebbe5c9653c9188
rubbie kelvin <dev.rubbie@gmail.com>
2021-03-13 12:54:10 +0100

    fixed codec problem

--- core/__init__.py
+++ core/__init__.py
@@ -6,7 +6,7 @@ from .main.routes import main
 def create_app():
 	app = Flask(__name__)
 
-	app.config["SECRET_KEY"] = "IgF//\\O6\\o75\\xdO\x\\d7DFo68Frtly;9FDDOdlf;7fD6DOF8F7f"
+	app.config["SECRET_KEY"] = "%E*&DI%6s6di5di5idi5u6su65dI75diTDdtTYDKdi56"
 	app.config["MONGO_URI"] = "mongodb+srv://me:i88uXJaR6RKTWhoX@audio-filed-cluster.ixpvo.mongodb.net/audio_filed_db?retryWrites=true&w=majority"
 
 	mongo.init_app(app)
